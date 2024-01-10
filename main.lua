6f9b85a48859d7a9b069687e234569f085b14b81
GeroChuu <156046341+GeroChuu@users.noreply.github.com>
2024-01-10 12:11:23 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,32 @@
 require'minimal_widget'
 local gui=require'minigui'()
+gui.debug=true
 local g=love.graphics
 
-local vel=500
-local runner=gui.Button("CLICK ME if you can..!", -180,120,180,40)
+local run=gui.Button("CLICK ME if you can..!", 180,120,180,40)
 function love.update(dt)
-    if runner.x>g.getWidth() or runner.x<-180 then vel=vel*-1 end
     gui:set_dt(dt)
-    if not gui:add(runner).toggle then
-        runner.x=runner.x+vel*dt
+    local runs=gui:add(run)
+    if run.doubleClick then
+        gui:debug_log("AH run")
+    end
+    if runs.focusLost then
+        gui:debug_log("IH run")
+    end
+    local ners=gui:add(gui.Button("CLICK ME if you can..!", 280,120,180,40):setID(420))
+    if ners.focusGained then
+        gui:debug_log("AH ner")
+    end
+    if ners.focusLost then
+        gui:debug_log("IH ner")
     end
 end
 function love.draw()
     gui:draw()
 end
+if love.system.getOS()=="Windows" then
 function love.mousepressed(...) gui:mousepressed(...) end
 function love.mousereleased(...)gui:mousereleased(...)end
+else
+function love.touchreleased(...)gui:touchreleased(...)end
+end
